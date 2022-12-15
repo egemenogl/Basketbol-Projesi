395e7dbf6f0d54b9f6376c984916289e336310ab
egemenogl <115708108+egemenogl@users.noreply.github.com>
2022-12-15 23:57:45 +0300

    Create main.py
    ilk commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import math
+import pygame
+import os
+from math import atan, pi
+
+DEBUG_MODE = False
+
+PLAY_SPEED = 10
+MOUSE_DIST_D = 5
+
+WIDTH = 1200   # Width : 6.75m
+WIDTH_IN_METER = 6.75
+HEIGHT = 800
+APP_NAME = "Eğik Atış"
+FPS = 60
+BALL_WIDTH = 50
+BALL_HEIGHT = 50
+BALL_START_X = 30
+BALL_START_Y = 340
+HOOP_X = 1100
+HOOP_Y = 600
